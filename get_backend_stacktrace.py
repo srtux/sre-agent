b22b71154972a0768aa1a387a1fef672e36a61ec
srtux <srtuxx@gmail.com>
2026-02-14 11:55:44 -0500

    chore: remove root-level test scripts containing project-specific information

--- get_backend_stacktrace.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import logging
-
-client = logging.Client(project="summitt-gcp", _http=None)
-filter_str = 'resource.type="aiplatform.googleapis.com/ReasoningEngine" AND textPayload=~"google.genai.errors.ClientError: 401 UNAUTHENTICATED"'
-for entry in client.list_entries(
-    filter_=filter_str, max_results=1, order_by=logging.DESCENDING
-):
-    payload = entry.payload or entry.text_payload
-    print(payload)
--- test_adk_client.py
+++
@@ -1,9 +0,0 @@
-from google.adk.memory import VertexAiMemoryBankService
-
-service = VertexAiMemoryBankService(
-    project="221980738988",
-    location="us-central1",
-    agent_engine_id="projects/221980738988/locations/us-central1/reasoningEngines/4168506966131343360",
-)
-client = service._get_api_client()  # type: ignore
-help(client.agent_engines.memories.create)
